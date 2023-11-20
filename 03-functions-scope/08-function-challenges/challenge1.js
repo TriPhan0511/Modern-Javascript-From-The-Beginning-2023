11c4024a0f68b493758b1a29d7ba53bbd8969ab8
Tri Phan <phanphutri@gmail.com>
2023-11-20 10:03:45 +0700

    Finished "03-functions-scope"

--- 03-functions-scope/08-function-challenges/challenge1.js
+++ 03-functions-scope/08-function-challenges/challenge1.js
@@ -1,6 +1,6 @@
 // ***** Challenge 1 *****
 const getCelsius = (temp) => Math.round(((temp - 32) * 5) / 9)
-console.log(`The temperature is ${getCelsius(32)} \xB0C`)
+// console.log(`The temperature is ${getCelsius(32)} \xB0C`)
 
 // ***** Challenge 2 *****
 // const minMax = (numbers) => {
@@ -39,10 +39,16 @@ const minMax = (numbers) => {
   return { min, max }
 }
 
-console.log(minMax([1, 2, 3, 4, 5]))
+// console.log(minMax([1, 2, 3, 4, 5]))
 // { min: 1, max: 5 }
 
 // console.log(minMax([1, 2, 0, 3, 10, 4, 5]))
 // // { min: 0, max: 10 }
 
 // ***** Challenge 3 *****
+;((length, width) =>
+  console.log(
+    `The area of a rectangle with a length of ${length} and a width of ${width} is ${
+      length * width
+    }.`
+  ))(10, 5)
