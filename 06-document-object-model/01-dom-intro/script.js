6db8913755e6b5deb625d13595b3279b89656131
Tri Phan <phanphutri@gmail.com>
2023-11-21 17:25:24 +0700

    Updated

---
+++ 06-document-object-model/01-dom-intro/script.js
@@ -0,0 +1,25 @@
+// console.log(window)
+// console.dir(window.document)
+
+// console.log(document)
+// console.dir(document.body)
+// console.log(document.body.innerHTML)
+// console.log(document.body.innerText)
+
+// const links = document.querySelectorAll('a')
+// console.log(links)
+// console.log(links[0].text)
+// console.log(links[0].href)
+
+// console.log(document.links)
+// console.log(document.links[0])
+
+// document.body.innerHTML = '<h1>Hello World</h1>'
+
+// document.write('Hello from JS')
+
+// console.log(document.getElementById('main'))
+// const main = document.getElementById('main')
+// console.log(main)
+// main.innerHTML = '<h1>Hello from main</h1>'
+document.querySelector('#main h1').innerText = 'Hello'
