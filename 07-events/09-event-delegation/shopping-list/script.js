d2461e11dd2b19a522d4817e4a8fbfd1c8b86632
Tri Phan <phanphutri@gmail.com>
2023-11-24 20:01:13 +0700

    Finished 07-events

--- 07-events/09-event-delegation/shopping-list/script.js
+++ 07-events/09-event-delegation/shopping-list/script.js
@@ -1,19 +1,38 @@
 const list = document.getElementById('item-list')
+if (list) {
+  list.addEventListener('mouseover', (e) => {
+    if (e.target.tagName === 'LI') {
+      e.target.style.backgroundColor = 'orange'
+    }
+  })
+}
 
-list.addEventListener('click', (e) => {
-  console.log(e.target)
-})
-
-// const listItems = document.querySelectorAll('li')
-
-// listItems.forEach((item) => {
-//   item.addEventListener('click', (e) => {
-//     e.currentTarget.remove()
+// ---------------------------------------------
+// PROBLEM: Click icon "X" to remove a list item
+// Solution 1
+// Add the event listener to the parent of the target element
+// const list = document.getElementById('item-list')
+// if (list) {
+//   list.addEventListener('click', (e) => {
+//     if (e.target.tagName === 'I') {
+//       e.target.parentElement.parentElement.remove()
+//     }
 //   })
+// }
+
+// Solution 2
+// Add the event listener to the parent of the target element
+// const list = document.getElementById('item-list')
+// list.addEventListener('click', (e) => {
+//   const listItem = e.target.parentElement.parentElement
+//   console.log(listItem)
+//   if (e.currentTarget === listItem.parentElement) {
+//     listItem.remove()
+//   }
 // })
 
+// Solution 3 (bad)
 // const removeButtons = document.querySelectorAll('.remove-item')
-
 // removeButtons.forEach((button) => {
 //   button.addEventListener('click', (e) => {
 //     e.currentTarget.parentElement.remove()
