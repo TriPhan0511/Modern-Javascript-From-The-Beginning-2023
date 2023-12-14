4838cf7da89fa39b577dae05a22f5e85dbae8d3a
Tri Phan <phanphutri@gmail.com>
2023-12-14 18:36:27 +0700

    Updated

--- 13-oop-constructors-prototypes/09-oop-game-challenge/script.js
+++ 13-oop-constructors-prototypes/09-oop-game-challenge/script.js
@@ -1,16 +1,33 @@
-function Player(name, lvl = 1, points = 0) {
+function Player(name) {
   this.name = name
-  this.lvl = lvl
-  this.points = points
+  this.lvl = 1
+  this.points = 0
 }
 
-Player.prototype.gainXp = function(number){
-  if (number < 1 || number > 10) {
+Player.prototype.gainXp = function (xp) {
+  if (xp < 1 || xp > 10) {
     return
   }
+  this.points += xp
+  if (this.points >= 10) {
+    this.lvl++
+    this.points -= 10
+  }
+}
+
+Player.prototype.describe = function () {
+  return `${this.name} is level ${this.lvl} with ${this.points} experience points`
 }
 
 let player1 = new Player('Bob')
 let player2 = new Player('Alice')
 
-console.log(player1)
+player1.gainXp(5)
+player2.gainXp(7)
+player1.gainXp(3)
+player2.gainXp(2)
+player1.gainXp(8)
+player2.gainXp(4)
+
+console.log(player1.describe()) // Bob is level 2 with 6 experience points
+console.log(player2.describe()) // Alice is level 2 with 3 experience points
